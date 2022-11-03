260ac45eab2174c016b7d0055eaacab89c17a3bc
saba <sabahusain2001@gmail.com>
2022-11-03 19:30:14 +0530

    printing hello world

---
+++ zedshaw/hello_world.py
@@ -0,0 +1,7 @@
+#This is my first program in python
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print('Yay! printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this')
