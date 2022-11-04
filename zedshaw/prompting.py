ab7cd3f7c9a071cc713bf2056e97bd476416e640
saba <sabahusain2001@gmail.com>
2022-11-04 16:31:55 +0530

    Prompting people

---
+++ zedshaw/prompting.py
@@ -0,0 +1,12 @@
+#prompting people
+#input as string
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+#input as int
+a = int(input("enter an integer(a): "))
+b = int(input("enter an integer(b): "))
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+print("a + b = ", a + b)
