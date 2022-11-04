71ec1b0d499412f9308abba23d94dfb35b0683f1
saba <sabahusain2001@gmail.com>
2022-11-04 16:29:06 +0530

    Print with escape character

---
+++ zedshaw/escape_sequence.py
@@ -0,0 +1,19 @@
+#how to use escape sequence character
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+tall_cat = 'I am 6\'2" tall'
+
+fat_cat = '''
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+'''
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(tall_cat)
+print(fat_cat)
