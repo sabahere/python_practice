7c4e7e7c9bc0d6623e0df8565128e567c39f7f2e
saba <sabahusain2001@gmail.com>
2022-11-04 16:28:22 +0530

    More problem on printing and formatting

---
+++ zedshaw/printing_printing_printing.py
@@ -0,0 +1,12 @@
+#some more printing tips on python
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul"
+
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+print("""
+I can print as much line as I want
+without using double quotes in 
+each line """)
