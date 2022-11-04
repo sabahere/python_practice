11e710afd02f24127eb1860bf5cfd65b749fe3bd
saba <sabahusain2001@gmail.com>
2022-11-04 16:27:49 +0530

    Printing Printing

---
+++ zedshaw/printing_printing.py
@@ -0,0 +1,12 @@
+#more printing and formatting
+formatter = "{} {} {} {}"
+
+print(formatter.format(1, 2, 3, 4))
+print(formatter.format("one", "two", "three", "four"))
+print(formatter.format(True, False, False, True))
+print(formatter.format(formatter, formatter, formatter, formatter))
+print(formatter.format("",
+        "I wish to",
+        "Die",
+        "Sooner than him"
+))
