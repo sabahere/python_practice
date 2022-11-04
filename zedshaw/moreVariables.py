7084f366ecc826a0de7e2df54a1dcc522a46937f
saba <sabahusain2001@gmail.com>
2022-11-04 16:14:32 +0530

    More variables and printing

---
+++ zedshaw/moreVariables.py
@@ -0,0 +1,23 @@
+#writing another program about variables of string
+
+#my name
+my_name = 'Saba Hussain'
+#my age
+my_age = 21
+#my height
+my_height = 62 #in inches
+#my_eyes
+my_eyes = 'brown'
+#my teeth
+my_teeth = 'white'
+#my hair
+my_hair = 'brown'
+
+print(f"Let's talk about {my_name}.")
+print(f"She is {my_height/12} feet tall.")
+print(f"She's got {my_eyes} eyes and {my_hair} hair.")
+print(f"Her teeth are usually {my_teeth} depending on the coffee.")
+
+total = my_age + my_height
+#adding my age and height and printing it
+print(f"If I add {my_age} and {my_height}, I get {total}.")
